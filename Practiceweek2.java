85feec261146c84822a58770acc56d51b164587c
Jerminej01 <60441030+Jerminej01@users.noreply.github.com>
2020-02-09 23:18:28 -0500

    Update Practiceweek2.java

--- Practiceweek2.java
+++ Practiceweek2.java
@@ -1,4 +1,30 @@
-
+/**
+*
+* @author  Jermine
+* Created: 2.3.20
+* Description: Variable Practice
+*
+**/
 public class Practiceweek2 {
+  
+  //Define variables
+  
+  double wallet = 10.35 ;
+  byte money = 5 ;
+  int cards = 3 ;
+  
+  //Declare 3 string variables.
+  
+  String beverage ;
+  String food ;
+  String snack ; {
+    
+  //Assign values to variables.
+    
+  beverage = "water" ;
+  food =     "mac & cheese" ;
+  snack =    "doritos" ;
+  
+  }  
 
 }
