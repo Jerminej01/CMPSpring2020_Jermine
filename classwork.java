04169bdf505e37a23ee6367efdde9be49a54b062
Jermine <60441030+Jerminej01@users.noreply.github.com>
2020-02-24 12:40:59 -0500

    Add files via upload

--- classwork.java
+++ classwork.java
@@ -1,35 +1,28 @@
-/** 
+/**
  * 
- * @author      Jermine
- * Created:     2.5.20
- * Description: This program calculates the product of 3 numbers.
- * 
- **/
-import java.util.Scanner ;
+ * @Jermine
+ * Date : 02/24/2020
+ *
+ */
+import java.util.Scanner;
 public class classwork {
-   public static void main(String[] args) {
-      
-    Scanner  keyboard = new Scanner(System.in) ;
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		
+		System.out.print("Enter a number: ");
+		int rn = kb.nextInt();
+		
+		if(rn==10)
+		for(int i=1; i<4; i+=1)
+		System.out.println("VICTORY"); 
+		
+		else if(rn!=10)
+		for(int i=1; i<4; i+=1)
+			System.out.println("GAME OVER");
+		
+		
+		
+	}
+	
 
-    int number1, number2, number3, answer ;
-      
-        System.out.println("The product of") ;
-   
-        System.out.print("First number: ") ;
-            number1 = keyboard.nextInt() ; 
-    
-        System.out.print("Second number: ") ;
-            number2 = keyboard.nextInt() ;
-   
-        System.out.print("Third number: ") ;
-            number3 = keyboard.nextInt() ;
-         
-            answer = number1 * number2 * number3 ;
-
-        System.out.println("Answer = " + answer) ; 
-
-
-   }
-        
-    }
-
+}
