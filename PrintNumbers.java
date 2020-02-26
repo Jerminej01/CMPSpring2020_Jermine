649572a02baea5644c068abd9fabf7dac052e601
Jermine <60441030+Jerminej01@users.noreply.github.com>
2020-02-26 12:48:13 -0500

    Add files via upload

---
+++ PrintNumbers.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author Jermine
+ *
+ */
+import java.util.Scanner;
+public class PrintNumbers {
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		
+		
+	for(int i=5; i<99; i++)
+		System.out.println(i);
+	}
+}
